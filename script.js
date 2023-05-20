c611a068c9833300b8243b7f1d9cf401b261e4c6
Bibhuti <biswalbibhuti8@gmail.com>
2023-05-21 00:18:49 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,65 +1,112 @@
-let users = [
-    {id: 1, name: "John", age: "18", profession: "developer"},
-    {id: 2, name: "Jack", age: "20", profession: "developer"},
-    {id: 3, name: "Karen", age: "19", profession: "admin"}
-];
+//intialising array
 
-function filterUsers() {
-    const t1=document.getElementById("profession");
-    const selectedPro=t1.value;
-    // console.log(t1.value);
-    // console.log(selectedPro);
-    if(selectedPro===""){
-        alert("Please select a profession ");
-    }
-    else{
-        const filteredUsers=users.filter((user)=>{
-            user.profession===selectedPro
-        });
-        console.log(filteredUsers);
-        displayUsers(filteredUsers);
-    }
+let arr = [
+    {
+        id:1,name:'john',age:'18',profession:'developer'
+    },
 
+    {
+        id:2, name:'jack',age:'20', profession:'developer'
+    },
     
-}
-
-function addUser(){
-    const name1=document.getElementById("name").value;
-    const age1=document.getElementById("age").value;
-    const prof1=document.getElementById("newProfession").value;
-
-    if(name1==="" || age1==="" || prof1===""){
-        alert("Enter all the details");
+    {
+            id:3, name:'karen', age:'19',profession:'admin'
     }
-    else{
-        const user1={
-            id: users.length+1,
-            name: name1,
-            profession: prof1,
-            age: age1
-        };
+]
 
-        users.push(user1);
+//displaying the array data
+let displaydiv = document.getElementById("display")
+
+
+function adddata(arr, index = 0) {
+    if (index >= arr.length) {
+      return;
     }
+  
+    let subdiv = document.createElement("div");
+    subdiv.className = "subdivclass";
+  
+    let id = document.createElement("div");
+    id.innerText = arr[index].id;
+  
+    let name = document.createElement("div");
+    name.innerText = "Name: " + arr[index].name;
+  
+    let age = document.createElement("div");
+    age.innerText = "Age: " + arr[index].age;
+  
+    let profession = document.createElement("div");
+    profession.innerText = "Profession: " + arr[index].profession;
+  
+    subdiv.append(id);
+    subdiv.append(name);
+    subdiv.append(profession);
+    subdiv.append(age);
+  
+    displaydiv.append(subdiv);
+  
+    adddata(arr, index + 1);
+  }
 
-}
+  adddata(arr,0)
 
-function displayUsers(){
-    const userList=document.getElementById("user-list");
-    // userList.innerHTML="Name : Bibhuti Profession : ASE age : 22";
-    // console.log(userList.innerHTML);
+
+
+
+
+// Function to filter users based on profession
+const filterBtn = document.getElementById('dropdown');
+
+function filterUsersByProfession() {
+    const selectedProfession = document.getElementById("dropdown").value;
+    const userList = document.getElementById("display");
     userList.innerHTML = "";
 
-    users.forEach(function(user){
-        const card=document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <p>ID: ${user.id}</p>
-            <p>Name: ${user.name}</p>
-            <p>Age: ${user.age}</p>
-            <p>Profession: ${user.profession}</p>
-        `;
-        userList.appendChild(card);
-    });
-
+   
+  
+    if (!selectedProfession) {
+      alert('Please select a profession.');
+      
+      return;
+    }
+  
+    const filteredUsers = arr.filter(user => user.profession.toLowerCase() === selectedProfession.toLowerCase());
+    
+    if (filteredUsers.length === 0) {
+        alert('select a profession before clicking the button');
+        adddata(arr);
+      } else {
+        adddata(filteredUsers);
+      }
 }
+
+
+
+//adding users 
+
+function addUser() {
+    const name = document.getElementById('name').value;
+    const profession = document.getElementById('newProfession').value;
+    const age = document.getElementById('age').value;
+  
+    if (!name || !profession || !age) {
+      alert('Please enter all the details.');
+      return;
+    }
+  
+    const newUser = {
+      id: arr.length + 1,
+      name: name,
+      profession: profession,
+      age: age
+    };
+  
+    arr.push(newUser);
+    displaydiv.innerHTML = ""; // Clear the existing content
+    adddata(arr);
+  
+    // Reset input fields
+    document.getElementById('name').value = '';
+    document.getElementById('newProfession').value = "";
+    document.getElementById('age').value = "";
+  }
